bb63d89a421e16c465d4e4f4e10c2f8639212bad
Daniel Morton <daniel@djmorton.com>
2017-12-09 17:50:19 -0500

    Add solution for day 5

---
+++ day5/part1/day5_part1.rb
@@ -0,0 +1,28 @@
+$instructions = []
+
+$current_instruction = 0
+$instruction_count = 0
+$step_counter = 0
+
+File.open("input.txt") do |file|
+  file.each_line do |line|
+    $instructions << line.to_i
+    $instruction_count += 1
+  end
+end
+
+def current_instruction_outside_bounds?
+  $current_instruction < 0 || $current_instruction >= $instruction_count
+end
+
+def execute_instruction
+  current_jump = $instructions[$current_instruction]
+  $instructions[$current_instruction] += 1
+  $current_instruction += current_jump
+  $step_counter += 1
+  return current_instruction_outside_bounds?
+end
+
+while !execute_instruction do end
+
+puts "#{$step_counter} steps taken"
